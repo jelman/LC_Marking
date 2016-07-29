44991cd2f9959b5cd057544071139f2a48dda005
jelman <jelman523@gmail.com>
2016-07-29 15:01:49 -0700

    Allow 5-8 slices. Add paths for use in cron job

--- copy_reorient_LC.sh
+++ copy_reorient_LC.sh
@@ -8,19 +8,24 @@
 # is newer than target.				#
 #################################################
 cwd=$PWD
+PATH=/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/lib/fsl/5.0
+. /etc/fsl/fsl.sh
+
+
+copy_fname="copy_reorient_"`date +%F`".log"
 
 # Find all TSE files and check if they have 8 slices before copying over
-cd /home/jelman/netshare/VETSA_NAS/mdv/4/data/MMILDB/VETSA3/proc
+cd /home/vetsatwinstudy/netshare/VETSA_NAS/mdv/4/data/MMILDB/VETSA3/proc
 for i in `find . -name "TSE2D*.nii"`;
 do
-
-	if [ $(fslhd $i | grep "^dim3" | awk '{print $2'}) -eq 8 ]; then 
-		rsync -RtOvu $i /home/jelman/netshare/VETSA_NAS/PROJ/LC_Marking/data; 
+    nslices=$(fslhd $i | grep "^dim3" | awk '{print $2'})
+	if [ $nslices -ge 5 ] && [ $nslices -le 8 ]; then 
+		rsync -RtOvu $i /home/vetsatwinstudy/netshare/VETSA_NAS/PROJ/LC_Marking/data >> /home/vetsatwinstudy/netshare/VETSA_NAS/PROJ/LC_Marking/data/$copy_fname; 
 	fi
 done
 
 # Reorient images to standard after checking that they do not already exist
-cd /home/jelman/netshare/VETSA_NAS/PROJ/LC_Marking/data
+cd /home/vetsatwinstudy/netshare/VETSA_NAS/PROJ/LC_Marking/data
 for i in `find -name "TSE2D*.nii"`; 
 do 
 	if [ ! -f "`dirname $i`/LC_FSE.nii.gz" ]; then
