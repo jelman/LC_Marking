bfa05b73b93f9e906c4861f298d116f7d0b6d273
jelman <jelman523@gmail.com>
2021-09-28 15:05:35 -0700

    Update lc_gather_cnr to automatically calculate al methods

--- lc_gather_cnr.py
+++ lc_gather_cnr.py
@@ -9,59 +9,61 @@ import argparse
 from glob import glob
 
 
-def summarise_cnr(subj_cnr_all, method='rostral2'):
+def summarise_cnr(subj_cnr_all):
     """
     Summarize CNR for a subject.
     Methods
     --------------
-        top2        :  Average of the top two slices (Default)
-        avg         :  Average of all three slices
-        max         :  Maximum value from the three slices
-        rostral2    :  Average of two most rostral slices
+        rostral2    :  Average of two most rostral slices 
+        avg3         :  Average of all three slices
+        max1         :  Maximum value from the three slices
         rostral1    :  Value of the single most rostral slice
         middle      :  Value of middle slice
         caudal1     :  Value of the single most caudal slice
     """
-    if method=='top2':
-        summ = subj_cnr_all.nlargest(2, 'CNR').mean()
-    elif method=='avg':
-        summ = subj_cnr_all.mean()
-    elif method=='max':
-        summ = subj_cnr_all.nlargest(1, 'CNR').mean()
-    elif method=='rostral2':
-        summ = subj_cnr_all.nsmallest(2, "Slice").mean()
-    elif method=='rostral1':
-        summ = subj_cnr_all.nsmallest(1, "Slice").mean()
-    elif method=='middle':
-        summ = subj_cnr_all.iloc[1,:]
-        summ.name = None    
-    elif method=='caudal1':
-        summ = subj_cnr_all.nlargest(1, "Slice").mean()
-    return summ.drop("Slice")
+    rostral2 = subj_cnr_all.nsmallest(2, "Slice").mean().drop("Slice")
+    rostral2.index = rostral2.index + "_rostral2"
+    
+    avg3 = subj_cnr_all.mean().drop("Slice")
+    avg3.index = avg3.index + "_avg3"
+    
+    max1 = subj_cnr_all.nlargest(1, 'CNR').mean().drop("Slice")
+    max1.index = max1.index + "_max1"
+    
+    rostral1 = subj_cnr_all.nsmallest(1, "Slice").mean().drop("Slice")
+    rostral1.index = rostral1.index + "_rostral1"
+
+    middle = subj_cnr_all.iloc[1,:].drop("Slice")
+    middle.index = middle.index + "_middle" 
+  
+    caudal1 = subj_cnr_all.nlargest(1, "Slice").mean().drop("Slice")
+    caudal1.index = caudal1.index + "_caudal1"
+        
+    return pd.concat([rostral2, avg3, max1, rostral1, middle, caudal1])
 
 
-def cnr_from_file(cnr_file, method):
+def cnr_from_file(cnr_file):
     """
     Extracts contrast values from a given subject in base directory.
     CNR file can be passed with or without file extension.
     """
     subj_cnr_all = pd.read_csv(cnr_file, sep='\t')
-    subj_cnr = summarise_cnr(subj_cnr_all, method)
+    subj_cnr = summarise_cnr(subj_cnr_all)
     return subj_cnr
 
 
-def get_all_subjs(filelist, method):
+def get_all_subjs(filelist):
     """ Iterates over all files to extract contrast estimates and append to a dataframe """
     all_subjs_cnr = pd.DataFrame()
     for subjfile in filelist:
         subject = os.path.split(os.path.dirname(subjfile))[-1]
-        subj_cnr = cnr_from_file(subjfile, method)
+        subj_cnr = cnr_from_file(subjfile)
         subj_cnr = pd.DataFrame(subj_cnr, columns=[subject]).T
         all_subjs_cnr = all_subjs_cnr.append(subj_cnr)
     return all_subjs_cnr
 
 
-def all_cnr_to_file(indir, cnrfile, outfile, method):
+def all_cnr_to_file(indir, cnrfile, outfile):
     """
     Searches through indir for cnrfiles and extracts LC CNR estimates.
     Saves to csv in base directory.
@@ -69,7 +71,7 @@ def all_cnr_to_file(indir, cnrfile, outfile, method):
     cnrfile = os.path.splitext(cnrfile)[0] + '.txt'
     globstr = os.path.join(indir, '*/' + cnrfile)
     filelist = glob(globstr)
-    all_subjs_cnr = get_all_subjs(filelist, method)
+    all_subjs_cnr = get_all_subjs(filelist)
     all_subjs_cnr.to_csv(outfile, index=True, index_label='SubjectID')
 
 
@@ -87,32 +89,17 @@ if __name__ == '__main__':
                         help='File name pattern with LC CNR values')
     parser.add_argument('-o', '--outfile', type=str, required=False,
                     help='Output filename. (default=<indir>/<cnrfile>_All.csv)')
-    methodgroup = parser.add_mutually_exclusive_group()
-    methodgroup.add_argument("--top2", action="store_const", dest="method",
-                             const="top2", help='Average of top 2 CNR values (default)')
-    methodgroup.add_argument("--avg", action="store_const", dest="method",
-                             const="avg", help='Average of all CNR values')
-    methodgroup.add_argument("--max", action="store_const", dest="method",
-                             const="max", help='Maxmimum CNR value only')
-    methodgroup.add_argument("--rostral2", action="store_const", dest="method",
-                             const="rostral2", default="rostral2",
-                             help='Average of the 2 most rostral slices')
-    methodgroup.add_argument("--rostral1", action="store_const", dest="method",
-                             const="rostral1", help='Value of the single most rostral slice')
-    methodgroup.add_argument("--middle", action="store_const", dest="method",
-                         const="middle", help='Value of the middle slice')
-    methodgroup.add_argument("--caudal1", action="store_const", dest="method",
-                         const="caudal1", help='Value of the single most caudal slice')
+
 
     if len(sys.argv) == 1:
         parser.print_help()
     else:
         args = parser.parse_args()
         if args.outfile is None:
-            outname = '_'.join([os.path.splitext(args.cnrfile)[0], args.method, "All.csv"])
+            outname = '_'.join([os.path.splitext(args.cnrfile)[0], "All.csv"])
             outfile = os.path.join(args.indir, outname)
         else:
             outfile = args.outfile
         ### Begin running script ###
-        all_cnr_to_file(args.indir, args.cnrfile, outfile, args.method)
+        all_cnr_to_file(args.indir, args.cnrfile, outfile)
 
